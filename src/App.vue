04d5209987e5daa6770d0970ca27ab0ef60f0f58
nemo0 <subhachanda88@gmail.com>
2022-12-13 14:32:19 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.svg" />
+    <Test msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import Test from "./components/Test.vue";
+
+export default {
+  name: "app",
+  components: {
+    Test,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 240px;
+}
+</style>
